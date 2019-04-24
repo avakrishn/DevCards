fa557857926264bb7fe9c541472faf7900d3577a
avakrishn <avakrishn@gmail.com>
2019-04-23 21:12:10 -0700

    delete unused files

--- db/schema_filter.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS filterCategories; 
-
-CREATE TABLE filterCategories(
-	users_id INT NOT NULL,
-    category VARCHAR (255) NOT NULL,
-	FOREIGN KEY (users_id) REFERENCES users(id)
-);
